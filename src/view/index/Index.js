4672c2ebfdac2648a66f308c5ca9c887a878fe83
ShawnGao <2519306859@qq.com>
2023-09-10 10:16:39 +0800

    :sparkles: Add a new home page.

---
+++ src/view/index/Index.js
@@ -0,0 +1,9 @@
+export const Index = () => {
+    return (
+        <header className="App-header">
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+        </header>
+    );
+}
