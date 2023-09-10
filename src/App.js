e1db34b553fd103b8fb734604b30c75cb616937f
ShawnGao <2519306859@qq.com>
2023-09-10 10:20:04 +0800

    :sparkles: Add new route.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import {Login} from "./view/login/Login";
+import {Index} from "./view/index/Index";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primereact/resources/primereact.css";
 import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
-    if (!localStorage.getItem("token")) {
-        return (
-            <div className="App">
-                <Login></Login>
-            </div>
-        );
-    }
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-            </header>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Index/>} />
+                    <Route path="/login" element={<Login/>} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
