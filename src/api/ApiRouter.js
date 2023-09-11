2f2aaa161ed792d2c99caae85fd18b6862f57823
ShawnGao <2519306859@qq.com>
2023-09-11 19:38:55 +0800

    :art: 将请求和实现放在同一个文件中.

--- src/api/ApiRouter.js
+++ src/api/ApiRouter.js
@@ -11,3 +11,15 @@ export const loginApi = (o) => {
   form.append("password", rsaEncrypt(o.password).toString());
   return post(userApi.login, form);
 }
+
+export const loginApiImpl = async (m, n, d) => {
+  const {response: r, data: dat} = await loginApi(d);
+  if (dat) {
+    localStorage.setItem("token", dat.token);
+    msg(m, "success", dat.message);
+    n("/portal/dashboard");
+  } else {
+    console.log(r);
+    msg(m, "error", r.data.exception);
+  }
+}
