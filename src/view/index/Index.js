1a65146c8a0ce6569c9924b8b9a236657a4b72a5
ShawnGao <2519306859@qq.com>
2023-09-11 19:40:05 +0800

    :fire: 删除一些文件.

--- src/view/index/Index.js
+++
@@ -1,9 +0,0 @@
-export const Index = () => {
-    return (
-        <header className="App-header">
-            <p>
-                Edit <code>src/App.js</code> and save to reload.
-            </p>
-        </header>
-    );
-}
